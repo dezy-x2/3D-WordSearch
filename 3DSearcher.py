de5b94ef3fab9c71554cb109baefa09ae4c1c7c9
Daniel Desmond <df.dezmond9@gmail.com>
2021-10-25 11:24:16 -0400

    func to find out which indexes need to be checked

--- 3DSearcher.py
+++ 3DSearcher.py
@@ -37,3 +37,21 @@ def binarySearch(arr, target):
             right = midIdx
         elif ord(target) > ord(arr[midIdx]):
             left = midIdx + 1
+
+# Now we need to traverse through the puzzle and find words that match the letters in the puzzle
+# This will probably best be executed via BFS checking all of the letters around the current letter
+# From there we will just check until we find all of the words
+# gonna be tricky
+
+
+def indexesToCheck(x, y, z):
+    indexes = [[x, y, z-1]]
+    if x > 0:
+        indexes.append([x-1, y, z])
+    if x < 2:
+        indexes.append([x+1, y, z])
+    if y > 0:
+        indexes.append([x, y-1, z])
+    if y < 2:
+        indexes.append([x, y+1, z])
+    return indexes
